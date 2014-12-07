be4c2798d2ee90d5c364ce8e7633d6a49bb7c7f1
Igor Pavlov <support@7-zip.org>
2014-12-07 23:50:44 +0800

    9.35 beta      2014-12-07 ------------------------- - 7zr.exe now support AES encryption. - SFX mudules were added to LZMA SDK - Some bugs were fixed.
    md5: 47ae1054f95c1ac878c09a6d44945aba lzma935.7z
    sha1: bc481ea7f4fcc79ca5e6cfe14b87b01254fc97d3 lzma935.7z

--- C/7zVersion.h
+++ C/7zVersion.h
@@ -1,8 +1,10 @@
 #define MY_VER_MAJOR 9
-#define MY_VER_MINOR 22
+#define MY_VER_MINOR 35
 #define MY_VER_BUILD 00
-#define MY_VERSION "9.22 beta"
-#define MY_7ZIP_VERSION "9.22 beta"
-#define MY_DATE "2011-04-18"
+#define MY_VERSION "9.35 beta"
+// #define MY_7ZIP_VERSION "9.35"
+#define MY_DATE "2014-12-07"
+#undef MY_COPYRIGHT
+#undef MY_VERSION_COPYRIGHT_DATE
 #define MY_COPYRIGHT ": Igor Pavlov : Public domain"
 #define MY_VERSION_COPYRIGHT_DATE MY_VERSION " " MY_COPYRIGHT " : " MY_DATE
